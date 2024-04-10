1a7e8d4e2ca88fb4c0c2ce48a4820ec439d7fc23
David-Lucian Dragan <david-lucian.dragan@htl.rennweg.at>
2024-04-10 10:20:19 +0200

    -

--- src/dbal.php
+++ src/dbal.php
@@ -1,1 +1,15 @@
 <?php
+
+require_once "../vendor/autoload.php";
+use Doctrine\DBAL\DriverManager;
+
+
+$connectionParams = [
+    'dbname' => 'tournaments',
+    'user' => 'root',
+    'password' => '',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql'
+];
+$conn = DriverManager::getConnection($connectionParams);
+
